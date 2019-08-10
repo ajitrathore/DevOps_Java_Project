114404d6fc8f0b3077a130a9e493644c83e8e8f6
ajitrathore <ajit.rathore@gmail.com>
2019-08-09 21:40:03 -0500

    Hi from eclipse

--- src/main/java/com/example/DevOps_Java_project/DevOpsJavaProjectApplication.java
+++ src/main/java/com/example/DevOps_Java_project/DevOpsJavaProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevOpsJavaProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("HI");
+		System.out.println("Hi There");
 		SpringApplication.run(DevOpsJavaProjectApplication.class, args);
 	}
 
