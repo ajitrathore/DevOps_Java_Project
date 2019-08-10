ae30ac00b67a6557605b7c834f949e926c4640a1
ajitrathore <ajit.rathore@gmail.com>
2019-08-09 21:20:08 -0500

    Java Code

---
+++ src/main/java/com/example/DevOps_Java_project/DevOpsJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevOps_Java_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsJavaProjectApplication.class, args);
+	}
+
+}
